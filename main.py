e38c342d523b537d08bc069517bf8b2363a5f9ef
alormada <adam.rola110@gmail.com>
2025-03-30 00:02:09 +0100

    add logic and time stamps

--- main.py
+++ main.py
@@ -16,27 +16,27 @@ cookie_button = driver.find_element(By.XPATH, value='//*[@id="cookie"]')
 beginning_time = time.time()
 timeout = beginning_time + 60*5
 
-# while time.time() < timeout:
-#     current_time = time.time()
-#     if current_time >= beginning_time + 5:
-#
-#     cookie_button.click()
+while time.time() < timeout:
+    current_time = time.time()
+    if current_time >= beginning_time + 5:
+        right_panel = driver.find_element(By.ID, value="store").find_elements(By.CSS_SELECTOR, value="b")
+        buttons = driver.find_elements(By.XPATH, "//div[@id='store']/div[@onclick]")
+        for i in range(len(right_panel) - 2, -1, -1):
+            n = int(right_panel[i].text.strip().replace(",", "").split("-")[1])
+            your_money = int(driver.find_element(By.XPATH, value='//*[@id="money"]').text.replace(",", ""))
+            button = buttons[i]
+            if n <= your_money:
+                # print(f"\n\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n{right_panel[i].text}\n\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n\n{button.text}\n\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+                button.click()
+                break
+            # print("Current button:", button.text)
+            # print("Price:", n)
+            # print("Your money", your_money)
 
+        beginning_time = time.time()
+    cookie_button.click()
 
-while True:
-    right_panel = driver.find_element(By.ID, value="store").find_elements(By.CSS_SELECTOR, value="b")
-    buttons = driver.find_element(By.ID, value="store").find_elements(By.CSS_SELECTOR, value="div")
-    for i in range(len(right_panel) - 2, -1, -1):
-        print(i)
-        n = int(right_panel[i].text.strip().replace(",","").split("-")[1])
-        your_money = int(driver.find_element(By.XPATH, value='//*[@id="money"]').text.replace(",", ""))
-        button = buttons[i]
-        if n <= your_money:
-            button.click()
-            break
-        print(n)
-        print(your_money)
-    time.sleep(2)
-
+cookies_per_second = driver.find_element(By.ID, value="cps").text
+print(f"Final result: {cookies_per_second} cookies/second")
 
 driver.quit()
